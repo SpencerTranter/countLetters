cd5776296f72f16d9509492d5fcc3266ceb44774
Spencer Tranter <spencertranter95@gmail.com>
2016-09-06 22:49:37 +0000

    Created function to find unique characters

--- countLetters.js
+++ countLetters.js
@@ -1,14 +1,25 @@
-function countLetters(word){
-  var splitWordArray = word.split('');
+var args = process.argv.slice(2);
+
+function countLetters(args){
+  var splitWordArray = args[0].split('');
   var countArray = letterCount(splitWordArray);
+  var uniqueArray = findUnique(splitWordArray);
+
+  // var countObject = {
+  //   row: function(splitWordArray, countArray) {
+
+  //   }
+  // }
 }
 
-function letterCount(wordArray){
+function letterCount(wordArray) {
   var countArray = [];
-  for(var i = 0; i < wordArray.length; i++){
+
+  for(var i = 0; i < wordArray.length; i++) {
     count = 0;
-    for(var x = 0; x < wordArray.length; x++){
-      if(wordArray[i] === wordArray[x]){
+
+    for(var x = 0; x < wordArray.length; x++) {
+      if(wordArray[i] === wordArray[x]) {
         count ++;
       }
     }
@@ -18,7 +29,29 @@ function letterCount(wordArray){
   return countArray;
 }
 
-countLetters('hello');
+function findUnique(wordArray) {
+  var uniqueArray = []
+
+  for(var i = 0; i < wordArray.length; i++) {
+    count = 0;
+
+    for(var x = 0; x < wordArray.length; x++) {
+      if(wordArray[i] === wordArray[x]){
+        count ++;
+      }
+    }
+
+    if (count === 1){
+      uniqueArray.push(wordArray[i]);
+    } else {
+      wordArray[i] = null;
+    }
+  }
+  console.log(uniqueArray);
+}
+
+
+countLetters(args);
 
 
 
