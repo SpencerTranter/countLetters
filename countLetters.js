c5e706f6882e004276cc759d295c16b48b745f9a
Spencer Tranter <spencertranter95@gmail.com>
2016-09-06 23:26:54 +0000

    Created function that turns string/num pairing array into string

--- countLetters.js
+++ countLetters.js
@@ -2,15 +2,48 @@ var args = process.argv.slice(2);
 
 function countLetters(args){
   var splitWordArray = args[0].split('');
-  //var countArray = letterCount(splitWordArray);
-  //var uniqueArray = findUnique(splitWordArray);
   var finalArray = createFinalArray(splitWordArray);
+  var objectString = createObjectString(finalArray);
 
-  // var countObject = {
-  //   row: function(splitWordArray, countArray) {
+}
 
-  //   }
-  // }
+function createFinalArray(wordArray){
+  var countArray = letterCount(wordArray);
+  var finalArray = [];
+
+  for(var i = 0; i < wordArray.length; i++) {
+    count = 0;
+
+    for(var x = 0; x < wordArray.length; x++) {
+      if(wordArray[i] === wordArray[x]){
+        count ++;
+      }
+    }
+    if (count === 1){
+      finalArray.push(wordArray[i]);
+      finalArray.push(countArray[i]);
+    } else {
+      wordArray[i] = null;
+      countArray[i] = null;
+    }
+  }
+  console.log(finalArray);
+  return finalArray;
+}
+
+function createObjectString(finalArray){
+  objectString = '';
+  for(var i = 0; i < finalArray.length; i ++) {
+    if(i === finalArray.length - 1) {
+      objectString += finalArray[i];
+    } else if(i % 2 === 0) {
+      objectString += finalArray[i] + ': ';
+    } else {
+      objectString += finalArray[i] + ', '
+    }
+  }
+  console.log(objectString);
+  return objectString;
 }
 
 function letterCount(wordArray) {
@@ -30,52 +63,28 @@ function letterCount(wordArray) {
   return countArray;
 }
 
-function findUnique(wordArray) {
-  var uniqueArray = [];
 
-  for(var i = 0; i < wordArray.length; i++) {
-    count = 0;
+// function findUnique(wordArray) {
+//   var uniqueArray = [];
 
-    for(var x = 0; x < wordArray.length; x++) {
-      if(wordArray[i] === wordArray[x]){
-        count ++;
-      }
-    }
+//   for(var i = 0; i < wordArray.length; i++) {
+//     count = 0;
 
-    if (count === 1){
-      uniqueArray.push(wordArray[i]);
-    } else {
-      wordArray[i] = null;
-    }
-  }
-  console.log(uniqueArray);
-  return uniqueArray;
-}
+//     for(var x = 0; x < wordArray.length; x++) {
+//       if(wordArray[i] === wordArray[x]){
+//         count ++;
+//       }
+//     }
 
-function createFinalArray(wordArray){
-  var countArray = letterCount(wordArray);
-  var finalArray = [];
-
-  for(var i = 0; i < wordArray.length; i++) {
-    count = 0;
-
-    for(var x = 0; x < wordArray.length; x++) {
-      if(wordArray[i] === wordArray[x]){
-        count ++;
-      }
-    }
-
-    if (count === 1){
-      finalArray.push(wordArray[i]);
-      finalArray.push(countArray[i]);
-    } else {
-      wordArray[i] = null;
-      countArray[i] = null;
-    }
-  }
-  console.log(finalArray);
-  return finalArray;
-}
+//     if (count === 1){
+//       uniqueArray.push(wordArray[i]);
+//     } else {
+//       wordArray[i] = null;
+//     }
+//   }
+//   console.log(uniqueArray);
+//   return uniqueArray;
+// }
 
 countLetters(args);
 
