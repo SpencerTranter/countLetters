a8f6d5c5c62c0eafb5adf3cd90c96d10e6accbb1
Spencer Tranter <spencertranter95@gmail.com>
2016-09-07 00:04:11 +0000

    Created object that dynaically creates keys. Removed most other functions

--- countLetters.js
+++ countLetters.js
@@ -1,90 +1,20 @@
 var args = process.argv.slice(2);
 
-function countLetters(args){
-  var splitWordArray = args[0].split('');
-  var finalArray = createFinalArray(splitWordArray);
-  var objectString = createObjectString(finalArray);
+function countLetters(word){
+  var splitWord = word.join('').toLowerCase().split('');
 
-}
-
-function createFinalArray(wordArray){
-  var countArray = letterCount(wordArray);
-  var finalArray = [];
-
-  for(var i = 0; i < wordArray.length; i++) {
-    count = 0;
-
-    for(var x = 0; x < wordArray.length; x++) {
-      if(wordArray[i] === wordArray[x]){
-        count ++;
+  var obj = {};
+    for(var i = 0; i < splitWord.length; i++){
+      if (!obj.hasOwnProperty(splitWord[i])){
+        obj[splitWord[i]] = 1;
+      } else {
+        obj[splitWord[i]] ++;
       }
     }
-    if (count === 1){
-      finalArray.push(wordArray[i]);
-      finalArray.push(countArray[i]);
-    } else {
-      wordArray[i] = null;
-      countArray[i] = null;
-    }
-  }
-  console.log(finalArray);
-  return finalArray;
+  console.log(obj);
+
 }
 
-function createObjectString(finalArray){
-  objectString = '';
-  for(var i = 0; i < finalArray.length; i ++) {
-    if(i === finalArray.length - 1) {
-      objectString += finalArray[i];
-    } else if(i % 2 === 0) {
-      objectString += finalArray[i] + ': ';
-    } else {
-      objectString += finalArray[i] + ', '
-    }
-  }
-  console.log(objectString);
-  return objectString;
-}
-
-function letterCount(wordArray) {
-  var countArray = [];
-
-  for(var i = 0; i < wordArray.length; i++) {
-    count = 0;
-
-    for(var x = 0; x < wordArray.length; x++) {
-      if(wordArray[i] === wordArray[x]) {
-        count ++;
-      }
-    }
-    countArray.push(count);
-  }
-  console.log(countArray);
-  return countArray;
-}
-
-
-// function findUnique(wordArray) {
-//   var uniqueArray = [];
-
-//   for(var i = 0; i < wordArray.length; i++) {
-//     count = 0;
-
-//     for(var x = 0; x < wordArray.length; x++) {
-//       if(wordArray[i] === wordArray[x]){
-//         count ++;
-//       }
-//     }
-
-//     if (count === 1){
-//       uniqueArray.push(wordArray[i]);
-//     } else {
-//       wordArray[i] = null;
-//     }
-//   }
-//   console.log(uniqueArray);
-//   return uniqueArray;
-// }
 
 countLetters(args);
 
